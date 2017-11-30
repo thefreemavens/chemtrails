f721b046c5c172f4e3bd130b993d5fcee60bcc35
Maven <thefreemavens@gmail.com>
2017-11-30 20:22:19 +0200

    Update geojson center

--- source/_assets/js/solo/tracker.js
+++ source/_assets/js/solo/tracker.js
@@ -96,14 +96,98 @@
 
 
 
-var map;
+
+
+
+
+
+
+// // var map;
+
+// function initMap() {
+//   // Styles a map in night mode.
+//   map = new google.maps.Map(document.getElementById('map'), {
+//     center: {lat: 34.187, lng: -96.679}, // USA
+//     // center: {lat: 32.093773, lng: 33.420410}, // Israel zoom 9
+//     zoom: 5,
+//     mapTypeControl: false,
+//     streetViewControl: false,
+//     styles: [
+//       {elementType: 'geometry', stylers: [{color: '#6342FF'}]},
+//       {elementType: 'labels.text.stroke', stylers: [{visibility: 'off'}]},
+//       {elementType: 'labels.text.fill', stylers: [{color: '#9E98FF'}]},
+//       {
+//         featureType: 'administrative.locality',
+//         elementType: 'labels.text.fill',
+//         stylers: [{color: '#9E98FF'}]
+//       },
+//       {
+//         featureType: 'administrative.locality',
+//         elementType: 'labels.text.stroke',
+//         stylers: [{visibility: 'off'}]
+//       },
+//       {
+//         featureType: 'poi',
+//         stylers: [{visibility: 'off'}]
+//       },
+//       {
+//         featureType: 'road',
+//         stylers: [{visibility: 'off'}]
+//       },
+//       {
+//         featureType: 'transit',
+//         stylers: [{visibility: 'off'}]
+//       },
+//       {
+//         featureType: 'water',
+//         elementType: 'geometry',
+//         stylers: [{color: '#4D2CC8'}]
+//       }
+//     ]
+//   });
+
+//   // Load JSON Data
+//   var json = map.data.loadGeoJson('assets/json/google.json');
+
+//   var myMarker = '/assets/images/icons/ct-marker.png';
+//   var marker = new google.maps.Marker({
+//     position: myLatLng,
+//     map: map,
+//     icon: myMarker
+//   });
+
+//   // // Set mouseover event for each feature.
+//   // map.data.addListener('mouseover', function(event) {
+//   //   document.getElementById('info-box').textContent =
+//   //       event.feature.getProperty('country');
+//   // });
+
+// } // End initMap()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// The following example creates complex markers to indicate beaches near
+// Sydney, NSW, Australia. Note that the anchor is set to (0,32) to correspond
+// to the base of the flagpole.
 
 function initMap() {
-  // Styles a map in night mode.
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 34.187, lng: -96.679}, // USA
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10,
+    center: {lat: 22.821757, lng: -24.433594}, // West Africa
+    // center: {lat: 34.187, lng: -96.679}, // USA zoom 5
     // center: {lat: 32.093773, lng: 33.420410}, // Israel zoom 9
-    zoom: 5,
+    zoom: 3,
     mapTypeControl: false,
     streetViewControl: false,
     styles: [
@@ -139,17 +223,62 @@ function initMap() {
       }
     ]
   });
+  map.data.loadGeoJson('assets/json/google.json');
+  setMarkers(map);
 
-  // Load JSON Data
-  var json = map.data.loadGeoJson('assets/json/google.json');
+}
 
-  // Set mouseover event for each feature.
-  map.data.addListener('mouseover', function(event) {
-    document.getElementById('info-box').textContent =
-        event.feature.getProperty('country');
-  });
+// // Data for the markers consisting of a name, a LatLng and a zIndex for the
+// // order in which these markers should display on top of each other.
+// var beaches = [
+//   ['Bondi Beach', -33.890542, 151.274856, 4],
+//   ['Coogee Beach', -33.923036, 151.259052, 5],
+//   ['Cronulla Beach', -34.028249, 151.157507, 3],
+//   ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
+//   ['Maroubra Beach', -33.950198, 151.259302, 1]
+// ];
 
-} // End initMap()
+function setMarkers(map) {
+  // Adds markers to the map.
+
+  // Marker sizes are expressed as a Size of X,Y where the origin of the image
+  // (0,0) is located in the top left of the image.
+
+  // Origins, anchor positions and coordinates of the marker increase in the X
+  // direction to the right and in the Y direction down.
+  var image = {
+    url: '/assets/images/icons/ct-marker.png',
+    // This marker is 20 pixels wide by 32 pixels high.
+    size: new google.maps.Size(20, 32),
+    // The origin for this image is (0, 0).
+    origin: new google.maps.Point(0, 0),
+    // The anchor for this image is the base of the flagpole at (0, 32).
+    anchor: new google.maps.Point(0, 32)
+  };
+  // Shapes define the clickable region of the icon. The type defines an HTML
+  // <area> element 'poly' which traces out a polygon as a series of X,Y points.
+  // The final coordinate closes the poly by connecting to the first coordinate.
+  var shape = {
+    coords: [1, 1, 1, 20, 18, 20, 18, 1],
+    type: 'poly'
+  };
+  for (var i = 0; i < features.length; i++) {
+    var feature = features[i];
+    var marker = new google.maps.Marker({
+      position: {lat: feature[1], lng: feature[2]},
+      map: map,
+      icon: image,
+      shape: shape,
+      title: feature[0],
+      zIndex: feature[3]
+    });
+  }
+}
+
+
+
+
+
 
 
 
